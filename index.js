f194340ec6f85d5ec12a0c1d12a8f726f14021e4
Kapil <sharmaakapil01@gmail.com>
2023-04-21 14:09:52 +0530

    completed the challenge by completing the age calculation part using javascript.

--- index.js
+++ index.js
@@ -21,12 +21,17 @@ arrowBtn.addEventListener('click',(e)=>{
     const dayValue = day.value;
     const monthValue = month.value;
     const yearValue = year.value;
- 
-  
+    let flag = leapYear(yearValue);
+    if(flag)
+    days_in_month[1] = 29;
+    else 
+    days_in_month[1] = 28;
+
   if(!isNumber(dayValue) || !isNumber(monthValue)
      || !isNumber(yearValue) || dayValue < 1 || dayValue > days_in_month[monthValue-1]
-     || monthValue < 1 || monthValue > 12 || yearLength(yearValue) != 4 || yearValue > curYear)
+     || monthValue < 1 || monthValue > 12 || yearLength(yearValue) != 4 || yearValue > curYear )
   {
+     
      let inputs = document.querySelectorAll('input');
      [...inputs].forEach((input)=>
      {
@@ -67,9 +72,43 @@ arrowBtn.addEventListener('click',(e)=>{
     {
       yearErr.classList.add('not-show');
     }
+    let res = calculateAge(dayValue,monthValue,yearValue);
+    days.innerText = res[2];
+    months.innerText = res[1];
+    years.innerText = res[0];
   }
   
-})
+});
+
+function calculateAge(day,month,year)
+{
+   
+   let yearDiff = (curYear-1) - year;
+   let monthdiff = monthDiff(month);
+   let dayDiff =   curDay - day;
+   console.log(dayDiff);
+   if(dayDiff < 0) 
+   {
+    let val = 0;
+    if(curMonth == 0) val = days_in_month[11];
+    else val = days_in_month[curMonth-1];
+    console.log(val);
+    dayDiff = dayDiff + val;
+   }
+   console.log(dayDiff);
+   return [yearDiff,monthdiff,dayDiff];
+}
+function monthDiff(monthV)
+{
+  let cnt = 0;
+  while((monthV % 13) != curMonth)
+  {
+    cnt++;
+    monthV++;
+  }
+  return cnt;
+}
+
 
 function isNumber(n)
 {
@@ -82,6 +121,18 @@ function yearLength(n)
 {
     console.log(n);
     let cnt = 0;
-   
+    while(n != 0)
+    {
+      cnt++;
+      n = Math.floor(n / 10);
+    }
     return cnt;
+}
+
+function leapYear(n)
+{
+
+  if(n % 4 == 0 &&(n%100 != 0 || n % 400 == 0)) return true;
+  return false;
+
 }
